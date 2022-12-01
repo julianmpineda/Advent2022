6915f9d813028dac3fc934c9ebf6e51b689431ac
julianmpineda <82466536+julianmpineda@users.noreply.github.com>
2022-12-01 14:18:13 -0500

    Update day1.java
    Cleaned up debug printouts

--- day1.java
+++ day1.java
@@ -4,7 +4,6 @@ public class day1 {
     public static void main(String[] args) {
         int maxOne = -1;
         int totalCal = 0;
-        int count = 0;
 
         try {
             File myObj = new File("data1.txt");
@@ -13,14 +12,10 @@ public class day1 {
             while (myReader.hasNextLine()) {
                 String temp = myReader.nextLine();
                 if (!temp.equals("")) {
-                    System.out.print("new int");
                     totalCal += Integer.parseInt(temp);
-                    System.out.println (" " + totalCal);
                 } else {
-                    count++;
                     max = Math.max(totalCal, max);
                     totalCal = 0;
-                    System.out.println("elf " + count + " done");
                 }
             }
             myReader.close();
