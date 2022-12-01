06a192fea7cbc55fe566f435da94f54ff843eaf8
julianmpineda <82466536+julianmpineda@users.noreply.github.com>
2022-12-01 14:36:40 -0500

    Update day1.java
    Changes to accommodate Part 2:
    
    Changed keep total calories of each elf in a list instead of doing all calculations while the file is open.
    Discovered original method was not retaining the final elf, didn't affect Part 1 but was affecting Part 2 answer.
    Prints Part 1 while calculating Part 2.
    
    Sorted list would probably be better for part 2 vs nested for loop.

--- day1.java
+++ day1.java
@@ -2,11 +2,14 @@ import java.util.*;
 import java.io.*;
 public class day1 {
     public static void main(String[] args) {
-        int maxOne = -1;
+        int max = -1;
         int totalCal = 0;
+        int topThree = 0;
+        int index = 0;
+        List<Integer> totals = new ArrayList<Integer>();
 
         try {
-            File myObj = new File("data1.txt");
+            File myObj = new File("C:\\Users\\jpin3\\IdeaProjects\\AdventOfCode2022\\src\\day1.txt");
 
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
@@ -14,15 +17,35 @@ public class day1 {
                 if (!temp.equals("")) {
                     totalCal += Integer.parseInt(temp);
                 } else {
-                    max = Math.max(totalCal, max);
+                    totals.add(totalCal);
                     totalCal = 0;
                 }
             }
+            /* Adds anything left over, otherwise will be lost. */
+            totals.add(totalCal);
+            totalCal = 0;
+            
             myReader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        System.out.println(max);
+        for (int i = 0; i <= 2; i++) {
+            max = -1;
+            for (int j = 0; j < totals.size(); j++) {
+                /* if statement allows us to also set the index, Math.max does not easily allow */
+                if (totals.get(j) > max) {
+                    max = totals.get(j);
+                    index = j;
+                }
+            }
+            if (i == 0) {
+                System.out.println("Part 1: " + max);
+            }
+            totals.remove((totals.indexOf(max)));
+            topThree += max;
+        }
+
+        System.out.println("Part 2: " + topThree);
     }
 }
