7bcffd1a9419e0761145fe177a47f61df84f9dd9
julianmpineda <82466536+julianmpineda@users.noreply.github.com>
2023-05-12 14:46:37 -0400

    Update day9.java
    Part 2 Code. Could work with part 1, would just need to change knots to size 4, and change the K loop. Could tie this into a flag for easier switching.
    
    3 nested loops isn't ideal.
    Also need to either split or streamline the function, since it's essentially just 2 functions the way it's written.

--- day9.java
+++ day9.java
@@ -7,9 +7,9 @@ public class day9 {
 
         List<String> track = new ArrayList<String>();
         List<String[]> directions = new ArrayList<String[]>();
-        int[] posHead = new int[]{0,0};
-        int[] posTail = new int[]{0,0};
-
+        int[] posHead = new int[]{0, 0};
+        int[] posTail = new int[]{0, 0};
+        int[] knots = new int[20];
         track.add("0 0");
 
         try {
@@ -23,50 +23,99 @@ public class day9 {
         }
 
         for (int i = 0; i < directions.size(); i++) {
+
+            int[] temp = new int[2];
+            int[] tempHead = new int[2];
             String[] move = directions.get(i);
             int steps = Integer.parseInt(move[1]);
-            int[] temp = new int[4];
 
-            for (int j = 0; j< steps; j++){
-                temp = moveKnot(move[0], posHead, posTail);
-                String strPos = "" + temp[2] + " " + temp[3];
+            for (int j = 0; j < steps; j++) {
+                temp = moveKnot(move[0], true, new int[]{knots[0], knots[1]}, new int[]{knots[2], knots[3]});
+                knots[0] = temp[0];
+                knots[1] = temp[1];
+                
+                for (int k = 0; k < 17; k++) {
+                    temp = moveKnot(move[0], false, new int[]{knots[k], knots[k+1]}, new int[]{knots[k+2], knots[k+3]});
+                    knots[k + 2] = temp[0];
+                    knots[k + 3] = temp[1];
+                    k++;
+                }
+                String strPos = "" + knots[18] + " " + knots[19];
+                
                 if (!track.contains(strPos)) {
                     track.add(strPos);
                 }
             }
         }
+        
         System.out.println("sumTrack: " + track.size());
     }
-    public static int[] moveKnot(String move, int[] head, int [] tail) {
 
-        int direction = 0, axis = 0;
-        int[] headNew = new int[2];
+    public static int[] moveKnot(String move, boolean firstKnot, int[] head, int[] tail) {
+
+        int direction = 0, axis = 0, unAxis = 0;
+        int[] posNew = new int[2];
 
         switch (move) {
-            case "R" :
+            case "R":
                 direction = 1;
-                axis = 0;
+                unAxis = 1;
                 break;
             case "U":
                 direction = 1;
                 axis = 1;
-               break;
+                break;
             case "L":
                 direction = -1;
-                axis = 0;
+                unAxis = 1;
                 break;
             case "D":
                 direction = -1;
-                axis  = 1;
+                axis = 1;
                 break;
         }
-            headNew[axis] = head[axis] + direction;
-            if (Math.abs(headNew[axis] - tail[axis]) > 1) {
-                tail[0] = head[0];
-                tail[1] = head[1];
-            }
-            head[axis] = headNew[axis];
 
-        return new int[]{headNew[0], headNew[1], tail[0],tail[1]};
+        if (firstKnot) {
+
+            posNew[axis] = head[axis] + direction;
+            posNew[unAxis] = head[unAxis];
+
+            return new int[]{posNew[0], posNew[1]};
+
+        } else if ((Math.abs(head[0] - tail[0]) + Math.abs(head[1] - tail[1]) > 2)) {
+            if (head[0] > tail[0]) {
+                posNew[0] = tail[0] + 1;
+            } else if (head[0] < tail[0]) {
+                posNew[0] = tail[0] - 1;
+            }
+            if (head[1] > tail[1]) {
+                posNew[1] = tail[1] + 1;
+            } else if (head[1] < tail[1]) {
+                posNew[1] = tail[1] - 1;
+            }
+
+        } else {
+            if ((Math.abs(head[0] - tail[0])) > 1) {
+
+                if (head[0] > tail[0]) {
+                    posNew[0] = tail[0] + 1;
+                } else if (head[0] < tail[0]) {
+                    posNew[0] = tail[0] - 1;
+                }
+            } else {
+                posNew[0] = tail[0];
+            }
+
+            if (Math.abs(head[1] - tail[1]) > 1) {
+                if (head[1] > tail[1]) {
+                    posNew[1] = tail[1] + 1;
+                } else if (head[1] < tail[1]) {
+                    posNew[1] = tail[1] - 1;
+                }
+            } else {
+                posNew[1] = tail[1];
+            }
+        }
+        return new int[]{posNew[0], posNew[1]};
     }
 }
